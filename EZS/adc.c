c52d84d99f326b3a39fcb3b8091c8f81c77f319b
Simon Rainer <sr@mail25.de>
2017-07-18 09:57:02 +0200

    Added lib.c

--- EZS/adc.c
+++ EZS/adc.c
@@ -10,6 +10,7 @@
 #include <libopencm3/stm32/timer.h>
 
 #include "ezs_io.h"
+#include "lib.h"
 
 //#include "stm32f4xx_hal.h"
 
@@ -25,97 +26,40 @@ static cyg_handle_t polling_handle;
 void polling_thread(cyg_addrword_t arg)
 {
 
-	//cyg_uint32 pina0 = CYGHWR_HAL_STM32_PIN_IN(A, 0, PULLUP);
-	//cyg_uint32 pina1 = CYGHWR_HAL_STM32_PIN_IN(A, 1, PULLUP);
+    int success = initRotaryEncoderTimer(TIM2, GPIOA, GPIO5, GPIO_AF1, GPIOA, GPIO1, GPIO_AF1);
+    success = initRotaryEncoderTimer(TIM3, GPIOC, GPIO6, GPIO_AF2, GPIOC, GPIO7, GPIO_AF2);
+    success = initRotaryEncoderTimer(TIM4, GPIOD, GPIO12, GPIO_AF2, GPIOB, GPIO7, GPIO_AF2);
 
-	//gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO0);
-	//HAL_Init();
-	//SystemClock_Config();
-	//
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO5);
-	gpio_set_af(GPIOA, GPIO_AF1, GPIO5);
-
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO1);
-	gpio_set_af(GPIOA, GPIO_AF1, GPIO1);
-
-	rcc_periph_clock_enable(RCC_GPIOA);
-	//rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_TIM2);
-
-	timer_reset(TIM2);
-
-	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, //For dead time and filter sampling, not important for now.
-		       TIM_CR1_CMS_EDGE,	//TIM_CR1_CMS_EDGE
-						//TIM_CR1_CMS_CENTER_1
-						//TIM_CR1_CMS_CENTER_2
-						//TIM_CR1_CMS_CENTER_3 la frequencia del pwm se divide a la mitad.
-			 TIM_CR1_DIR_UP);
-
-	timer_set_prescaler(TIM2, 0);
-	timer_set_repetition_counter(TIM2, 0);
-	timer_enable_preload(TIM2);
-	timer_continuous_mode(TIM2);
-
-	timer_slave_set_mode(TIM2, TIM_SMCR_SMS_EM3); // encoder
-
-	timer_set_oc_polarity_high(TIM2, TIM_OC1);
-	timer_set_oc_polarity_high(TIM2, TIM_OC2);
-
-	timer_ic_disable(TIM2, TIM_IC1);
-	timer_ic_disable(TIM2, TIM_IC2);
-
-	timer_ic_set_input(TIM2, TIM_IC1, TIM_IC_IN_TI1);
-	timer_ic_set_input(TIM2, TIM_IC2, TIM_IC_IN_TI1);
-
-	timer_disable_oc_output(TIM2, TIM_OC1);
-	timer_disable_oc_output(TIM2, TIM_OC2);
-
-	timer_disable_preload_complementry_enable_bits(TIM2);
-
-	timer_set_period(TIM2, 0xFFFFFFFF);
-
-	timer_enable_counter(TIM2);
-
-	//timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-
-	//MX_GPIO_Init();
-	
-	//MX_TIM2_Init();
-	//timer_enable_counter(TIM2);
-
-	for (;;) {
-		//gpio_set(LED_DISCO_BLUE_PORT, LED_DISCO_BLUE_PIN);
-		//int val;
-		//CYGHWR_HAL_STM32_GPIO_IN(pin, &val);
-		ezs_printf("hello %d \n", (int)timer_get_counter(TIM2));
-	}
+    for (;;) {
+	ezs_printf("TIM2: %8d TIM3: %8d TIM4: %8d\n", (int)timer_get_counter(TIM2), (int) timer_get_counter(TIM3), (int) timer_get_counter(TIM4));
+    }
 }
 
 void cyg_user_start(void)
 {
-	//CYGHWR_HAL_STM32_GPIO_IRQ_INIT();
-	//cyg_uint32 pin = CYGHWR_HAL_STM32_PIN_IN(A, 0, PULLUP);
-	//cyg_bool enabled = CYGHWR_HAL_VAR_GPIO_IRQ_ENABLE(pin, CYGHWR_HAL_VAR_IRQ_EDGE_BOTH, cyg_hal_var_gpio_callback_t handler, NULL);
+    //CYGHWR_HAL_STM32_GPIO_IRQ_INIT();
+    //cyg_uint32 pin = CYGHWR_HAL_STM32_PIN_IN(A, 0, PULLUP);
+    //cyg_bool enabled = CYGHWR_HAL_VAR_GPIO_IRQ_ENABLE(pin, CYGHWR_HAL_VAR_IRQ_EDGE_BOTH, cyg_hal_var_gpio_callback_t handler, NULL);
 
 
-	/* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+    /* USER CODE END Init */
 
-	/* Configure the system clock */
+    /* Configure the system clock */
 
-	/* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
+    /* Initialize all configured peripherals */
 
-	/* Thread erzeugen ... */
-	cyg_thread_create(PRIO, &polling_thread, 0, "Pin polling thread", polling_stack, STACKSIZE, &polling_handle, &polling_data);
-	//cyg_thread_create(PRIO, &interrupt_thread, 0, "Pin interrupt thread", interrupt_stack, STACKSIZE, &interrupt_handle, &interrupt_data);
-	//cyg_thread_create(PRIO, &sin_thread, 0, "sin thread", sin_stack, STACKSIZE, &sin_handle, &sin_threaddata);
+    /* Thread erzeugen ... */
+    cyg_thread_create(PRIO, &polling_thread, 0, "Pin polling thread", polling_stack, STACKSIZE, &polling_handle, &polling_data);
+    //cyg_thread_create(PRIO, &interrupt_thread, 0, "Pin interrupt thread", interrupt_stack, STACKSIZE, &interrupt_handle, &interrupt_data);
+    //cyg_thread_create(PRIO, &sin_thread, 0, "sin thread", sin_stack, STACKSIZE, &sin_handle, &sin_threaddata);
 
-	/* Thread starten ... */
-	//cyg_thread_resume(interrupt_handle);
-	cyg_thread_resume(polling_handle);
+    /* Thread starten ... */
+    //cyg_thread_resume(interrupt_handle);
+    cyg_thread_resume(polling_handle);
 }
---
+++ EZS/src/tmp_lib.c
@@ -0,0 +1,78 @@
+#include <libopencm2/stm32/f4/rcc.h>
+#include "lib.h"
+
+int initRotaryEncoderTimer(uint32_t tim,  uint32_t portA, uint16_t pinA, uint8_t afA, uint32_t portB, uint16_t pinB, uint8_t afB) {
+	gpio_mode_setup(portA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, pinA);
+	gpio_set_af(portA, afA, pinA);
+
+	gpio_mode_setup(portB, GPIO_MODE_AF, GPIO_PUPD_PULLUP, pinB);
+	gpio_set_af(portB, afB, pinB);
+
+	enum rcc_periph rccTim = RCC_TIM2;
+	switch (tim) {
+	    case TIM1: rccTim = RCC_TIM1; break;
+	    case TIM2: rccTim = RCC_TIM2; break;
+	    case TIM3: rccTim = RCC_TIM3; break;
+	    case TIM4: rccTim = RCC_TIM4; break;
+	    case TIM5: rccTim = RCC_TIM5; break;
+	    case TIM9: rccTim = RCC_TIM9; break;
+	    default: return 1;
+	}
+
+	enum rcc_periph rccA;
+	switch (portA) {
+	    case GPIOA: rccA = RCC_GPIOA; break;
+	    case GPIOB: rccA = RCC_GPIOB; break;
+	    case GPIOC: rccA = RCC_GPIOC; break;
+	    case GPIOD: rccA = RCC_GPIOD; break;
+	    default: return 2;
+	}
+	enum rcc_periph rccb;
+	switch (portB) {
+	    case GPIOA: rccB = RCC_GPIOA; break;
+	    case GPIOB: rccB = RCC_GPIOB; break;
+	    case GPIOC: rccB = RCC_GPIOC; break;
+	    case GPIOD: rccB = RCC_GPIOD; break;
+	    default: return 2;
+	}
+
+	rcc_periph_clock_enable(rccA);
+	rcc_periph_clock_enable(rccB);
+	rcc_periph_clock_enable(rccTim);
+
+	timer_reset(tim);
+
+	timer_set_mode(tim, TIM_CR1_CKD_CK_INT, //For dead time and filter sampling, not important for now.
+		       TIM_CR1_CMS_EDGE,	//TIM_CR1_CMS_EDGE
+						//TIM_CR1_CMS_CENTER_1
+						//TIM_CR1_CMS_CENTER_2
+						//TIM_CR1_CMS_CENTER_3 la frequencia del pwm se divide a la mitad.
+			 TIM_CR1_DIR_UP);
+
+	timer_set_prescaler(tim, 0);
+	timer_set_repetition_counter(tim, 0);
+	timer_enable_preload(tim);
+	timer_continuous_mode(tim);
+
+	timer_slave_set_mode(tim, TIM_SMCR_SMS_EM3); // encoder
+
+	timer_set_oc_polarity_high(tim, TIM_OC1);
+	timer_set_oc_polarity_high(tim, TIM_OC2);
+
+	timer_ic_disable(tim, TIM_IC1);
+	timer_ic_disable(tim, TIM_IC2);
+
+	timer_ic_set_input(tim, TIM_IC1, TIM_IC_IN_TI1);
+	timer_ic_set_input(tim, TIM_IC2, TIM_IC_IN_TI1);
+
+	timer_disable_oc_output(tim, TIM_OC1);
+	timer_disable_oc_output(tim, TIM_OC2);
+
+	timer_disable_preload_complementry_enable_bits(tim);
+
+	timer_set_period(tim, 0xFFFFFFFF);
+
+	timer_enable_counter(tim);
+
+	return 0;
+}
